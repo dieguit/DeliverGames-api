997d4c248610b95c25ac5127b3820140649f4978
Diego Gil <diego.gil2505@gmail.com>
2018-02-27 19:42:44 -0300

    Created local config

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,27 @@
+import LocalConfig from './connection.local';
+
 const development = {
-  database: 'delivergames_api',
-  username: 'root',
-  password: '',
-  host: 'localhost',
-  dialect: 'mysql',
+  database: LocalConfig.development.database || 'delivergames_api',
+  username: LocalConfig.development.username || 'root',
+  password: LocalConfig.development.password || '',
+  host: LocalConfig.development.host || 'localhost',
+  dialect: LocalConfig.development.dialect || 'mysql',
 };
 
 const testing = {
-  database: 'databasename',
-  username: 'username',
-  password: 'password',
-  host: 'localhost',
-  dialect: 'mysql',
+  database: LocalConfig.testing.database || 'databasename',
+  username: LocalConfig.testing.username || 'username',
+  password: LocalConfig.testing.password || 'password',
+  host: LocalConfig.testing.host || 'localhost',
+  dialect: LocalConfig.testing.dialect || 'mysql',
 };
 
 const production = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'mysql',
+  database: LocalConfig.production.database || process.env.DB_NAME,
+  username: LocalConfig.production.username || process.env.DB_USER,
+  password: LocalConfig.production.password || process.env.DB_PASS,
+  host: LocalConfig.production.host || process.env.DB_HOST || 'localhost',
+  dialect: LocalConfig.production.dialect || 'mysql',
 };
 
 module.exports = {
